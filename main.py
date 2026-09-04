195d871cf86e41413c196f843137fd8a10a7c247
vibodh <vibodh08@gmail.com>
2026-09-04 22:22:35 +0530

    Add CORS for frontend

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from fastapi.middleware.cors import CORSMiddleware
 from datetime import date, datetime, timedelta
 import qrcode
 from fastapi.responses import StreamingResponse
@@ -21,6 +22,16 @@ Base.metadata.create_all(bind=engine)
 
 # Create FastAPI application
 app = FastAPI(title="HoneyChain API")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:8080",
+        "http://127.0.0.1:8080",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 # Password hashing
